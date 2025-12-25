68c8445a733ee8ddc202323be00e2cf761536af3
Oleg Rovneyko <oleg.rovneyko@gmail.com>
2025-12-26 00:08:46 +0200

    some changes

--- src/App.vue
+++ src/App.vue
@@ -38,25 +38,13 @@
     </div>
 
     <div>
-      <div v-for="postItem in posts" class="mb-4 p-4 bg-white border border-gray-200 flex justify-between items-end">
-        <div>          
-          <h3 class="text-lg text-gray-800 mb-2 border-b border-gray-200">{{ postItem.title }}</h3>
-          <p class="text-gray-600">{{ postItem.content }}</p>
-        </div>
-        <div class="flex item-center">
-          <svg @click="editPost(postItem)" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 mr-2 text-emerald-600 cursor-pointer">
-            <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
-          </svg>
-          <svg @click="deletePost(postItem)" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 text-red-600 cursor-pointer">
-            <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-          </svg>
-        </div>
-      </div>
+      <PostItem @postEdited="editPost" @postDeleted="deletePost" v-for="postItem in posts" :post="postItem"></PostItem>
     </div>
   </div>
 </template>
 
 <script>
+import PostItem from './components/post/PostItem.vue';
 export default {
   name: 'Our App',
 
@@ -66,7 +54,12 @@ export default {
       posts: [],
       editedPost: {},
       errors: [],
+      validatedFields: ['title', 'content'],
     }
+  },
+
+  components: {
+    PostItem,
   },
 
   methods: {
@@ -75,33 +68,33 @@ export default {
       this.posts.unshift(this.post);
       console.log(this.post)
       this.post = {};
-    },
-    deletePost(postItem) {
-      const index = this.posts.indexOf(postItem);
-      this.posts.splice(index, 1);
-    },
-    editPost(postItem) {
-      this.editedPost.index = this.posts.indexOf(postItem);
-      this.editedPost.title = postItem.title;
-      this.editedPost.content = postItem.content;
-    },
+    },    
     updatePost() {
       const post = this.posts[this.editedPost.index];
       post.title = this.editedPost.title;
       post.content = this.editedPost.content;
       this.editedPost = {};
     },
+    editPost(post) {
+      this.editedPost.index = this.posts.indexOf(post);
+      this.editedPost.title = post.title;
+      this.editedPost.content = post.content;
+    },
+    deletePost(post) {
+      const index = this.posts.indexOf(post);
+      this.posts.splice(index, 1);
+    },
     isValidated() {
       this.errors = [];
-      if (!this.post.title) {
-        this.errors.push('Title field is required');
-      }
-      if (!this.post.content) {
-        this.errors.push('Title field is required');
-      }
+      this.validatedFields.forEach(field => {
+        if (!this.post[field]) {
+          this.errors.push(`${field} field is required`);
+        }
+      });
       return this.errors.length < 1;
     }
   },
+
   watch: {
     post: {
       handler(newVal, oldVal) {
