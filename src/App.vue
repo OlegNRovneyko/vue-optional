7906dfe0719576a4cc375b5fbef3981761f0fd21
OlegNRovneyko <oleg.rovneyko@gmail.com>
2025-12-25 17:18:23 +0200

    init project

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="w-1/2 mx-auto p-4">
+    <div class="p-4 mb-4 bg-white border border-gray-200">
+      <div class="mb-4">
+        <input type="text" v-model="post.title" placeholder="title" class="border border-gray-200 p-4 w-full">
+      </div>
+      <div class="mb-4">
+        <textarea v-model="post.content" placeholder="content" class="border border-gray-200 p-4 w-full"></textarea>
+      </div>
+      <div class="mb-4">
+        <a href="#" @click.prevent="storePost" class="inline-block px-3 py-2 bg-sky-600 border border-sky-700 text-white">Add Post</a>
+      </div>
+    </div>
+
+    <div>
+      <div v-for="postItem in posts" class="mb-4 p-4 bg-white border border-gray-200 flex justify-between items-end">
+        <div>          
+          <h3 class="text-lg text-gray-800 mb-2 border-b border-gray-200">{{ postItem.title }}</h3>
+          <p class="text-gray-600">{{ postItem.content }}</p>
+        </div>
+        <div>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 cursor-pointer">
+            <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+          </svg>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Our App',
+
+  data() {
+    return {
+      post: {},
+      posts: [],
+    }
+  },
+
+  methods: {
+    storePost() {
+      this.posts.unshift(this.post);
+      console.log(this.post)
+      this.post = {};
+    }
+  }
+}
+</script>
+
+<style>
+  
+</style>
